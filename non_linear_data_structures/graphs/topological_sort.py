582b5ca565b2599308fa4d4b0c77a12827f22301
Manya Kaistha <manyakaistha.dev@gmail.com>
2025-02-27 15:33:07 +0530

    Implemented Topological Sort

---
+++ non_linear_data_structures/graphs/topological_sort.py
@@ -0,0 +1,23 @@
+def topological_sort(graph, start_vertex, visited=None, result=None):
+    if visited is None:
+        visited = set()
+    if result is None:
+        result = []
+
+    if start_vertex not in graph.graph:
+        return []
+
+    visited.add(start_vertex)
+
+    for neighbor in graph.graph[start_vertex]:
+        if neighbor not in visited:
+           topological_sort(graph, start_vertex, visited, result)
+
+    # result.insert(0, start_vertex) # This works but is ineffecient as we insert at the start of the list
+    # return result
+    result.append(start_vertex)
+
+    if len(result) == len(graph.graph):
+        result.reverse()
+
+    return result
