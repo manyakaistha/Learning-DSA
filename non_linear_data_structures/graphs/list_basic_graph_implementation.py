ccfcac9552e4b2637df02f63256827c80050e332
Manya Kaistha <manyakaistha.dev@gmail.com>
2025-02-27 16:18:53 +0530

    Added Directed Graphs and Topological Sort Using Kahn's Algorithm

--- non_linear_data_structures/graphs/list_basic_graph_implementation.py
+++ non_linear_data_structures/graphs/list_basic_graph_implementation.py
@@ -1,4 +1,67 @@
-class Graph:
+class WeightedGraph:
+    def __init__(self, directed=False):
+        self.graph = {}
+        self.directed = directed
+
+    def add_vertex(self, vertex):
+        if vertex not in self.graph:
+            self.graph[vertex] = []
+
+    def add_edge(self, vertex1, vertex2, weight=1):
+        self.add_vertex(vertex1)
+        self.add_vertex(vertex2)
+
+        for i, (v, w) in enumerate(self.graph[vertex1]):
+            if v == vertex2:
+                self.graph[vertex1][i] = (vertex2, weight)
+                break
+        else:
+            self.graph[vertex1].append((vertex2, weight))
+
+        if not self.directed:
+            for i, (v, w) in enumerate(self.graph[vertex2]):
+                if v == vertex1:
+                    self.graph[vertex2][i] = (vertex1, weight)
+                    break
+            else:
+                self.graph[vertex2].append((vertex1, weight))
+
+    def remove_vertex(self, vertex):
+        if vertex in self.graph:
+            for v in self.graph:
+                self.graph[v] = [(n, w) for n, w in self.graph[v] if n != vertex]
+            del self.graph[vertex]
+
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.graph and vertex2 in self.graph:
+            self.graph[vertex1] = [(v, w) for v, w in self.graph[vertex1] if v != vertex2]
+            if not self.directed:
+                self.graph[vertex2] = [(v, w) for v, w in self.graph[vertex2] if v != vertex1]
+
+    def get_vertices(self):
+        return list(self.graph.keys())
+
+    def get_edges(self):
+        edges = []
+        for vertex in self.graph:
+            for neighbor, weight in self.graph[vertex]:
+                if self.directed:
+                    edges.append((vertex, neighbor, weight))
+                else:
+                    edge = tuple(sorted([vertex, neighbor]) + [weight])
+                    if edge not in edges:
+                        edges.append(edge)
+        return edges
+
+    def get_weight(self, vertex1, vertex2):
+        if vertex1 in self.graph:
+            for neighbor, weight in self.graph[vertex1]:
+                if neighbor == vertex2:
+                    return weight
+        return None
+
+# the code below works for unweighted graphs
+class UnweightedGraph:
     def __init__(self, directed=False):
         self.graph = {}
         self.directed = directed
--- non_linear_data_structures/graphs/topological_sort.py
+++ non_linear_data_structures/graphs/topological_sort.py
@@ -15,7 +15,7 @@ def topological_sort(graph, start_vertex, visited=None, result=None):
 
     # result.insert(0, start_vertex) # This works but is ineffecient as we insert at the start of the list
     # return result
-    result.append(start_vertex)
+    result.append(start_vertex) #jsut append and only reverse the list when the whole graph has been traversed
 
     if len(result) == len(graph.graph):
         result.reverse()
